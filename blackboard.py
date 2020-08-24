6220b2208a5e0d211944b0f4112f131a3c0e9c67
Sheri98 <52729173+Sheri98@users.noreply.github.com>
2020-08-24 12:47:41 +0530

    Update blackboard.py

--- blackboard.py
+++ blackboard.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 import time
 import re
 import datetime
@@ -10,23 +11,33 @@ root.title("BlackBoard Login System")
 
 # Functionality start
 def blackboard():
-    driver = webdriver.Chrome(executable_path="D:\shareble\drivers\chromedriver.exe")
+    chrome_options = Options()
+    chrome_options.add_argument("--disable-user-media-security=true")
+    driver = webdriver.Chrome(executable_path="D:\shareble\drivers\chromedriver.exe", options=chrome_options)
     driver.get("https://cuchd.blackboard.com/")
-    driver.implicitly_wait(3)
+    driver.implicitly_wait(2)
     driver.maximize_window()
     driver.find_element_by_id("user_id").send_keys(username.get())
     driver.find_element_by_id("password").send_keys(password.get())
     driver.find_element_by_id("entry-login").click()
     x = datetime.datetime.now()
     times = re.search("((\d{2}[:])+\d{2})",str(x)).group(0)
-    print(times)
+    print(type(times))
     day = datetime.datetime.today().weekday()
     print(type(day))
+    time.sleep(5)
 
     if day == 0:
-        if times >= "10:00:00" and times <= "12:45:00":
+        print("Entered")
+        if str(times) >= "09:45:00" and times <= "12:45:00":
+            print("Entered")
             pbj = driver.find_element_by_xpath("/html/body/div[1]/div[2]/bb-base-layout/div/main/div/div/div[1]/div[1]/div/div/div[1]/div/div[2]/div/div[7]/bb-base-course-card/div[1]/div[2]/a/h4")
             driver.execute_script("arguments[0].scrollIntoView()",pbj)
+            driver.find_element_by_xpath("//*[@id='course-link-_5770_1']/h4").click()
+            driver.find_element_by_xpath("// *[ @ id = 'sessions-list-dropdown']").click()
+            time.sleep(10)
+            driver.find_element_by_xpath("//*[@id='sessions-list']/li[2]/a").click()
+
         elif times >= "13:30:00" and times < "14:30:00":
             pbj = driver.find_element_by_xpath("/html/body/div[1]/div[2]/bb-base-layout/div/main/div/div/div[1]/div[1]/div/div/div[1]/div/div[2]/div/div[5]/bb-base-course-card/div[1]/div[2]/a/h4")
             driver.execute_script("arguments[0].scrollIntoView()",pbj)
@@ -44,7 +55,7 @@ def blackboard():
             pbj = driver.find_element_by_xpath("/html/body/div[1]/div[2]/bb-base-layout/div/main/div/div/div[1]/div[1]/div/div/div[1]/div/div[2]/div/div[9]/bb-base-course-card/div[1]/div[2]/a/h4")
             driver.execute_script("arguments[0].scrollIntoView()",pbj)
     elif day == 2:
-        if times >= "10:00:00" and times < "10:45:00":
+        if times >= "09:45:00" and times < "10:45:00":
             pbj = driver.find_element_by_xpath("/html/body/div[1]/div[2]/bb-base-layout/div/main/div/div/div[1]/div[1]/div/div/div[1]/div/div[2]/div/div[8]/bb-base-course-card/div[1]/div[2]/a/h4")
             driver.execute_script("arguments[0].scrollIntoView()",pbj)
         elif times >= "10:45:00" and times < "11:45:00":
@@ -66,7 +77,7 @@ def blackboard():
             pbj = driver.find_element_by_xpath("/html/body/div[1]/div[2]/bb-base-layout/div/main/div/div/div[1]/div[1]/div/div/div[1]/div/div[2]/div/div[4]/bb-base-course-card/div[1]/div[2]/a/h4")
             driver.execute_script("arguments[0].scrollIntoView()",pbj)
     elif day == 5:
-        if  times >= "10:00:00" and times < "11:45:00":
+        if  times >= "09:45:00" and times < "11:45:00":
             pbj = driver.find_element_by_xpath("/html/body/div[1]/div[2]/bb-base-layout/div/main/div/div/div[1]/div[1]/div/div/div[1]/div/div[2]/div/div[6]/bb-base-course-card/div[1]/div[2]/a/h4")
             driver.execute_script("arguments[0].scrollIntoView()",pbj)
         elif times >= "13:30:00" and times <"14:30:00":
@@ -90,5 +101,4 @@ password.grid(row=1,column=1,columnspan=2)
 submit = Button(root,text="Enter",command=blackboard)
 submit.grid(row=2,column=0)
 
-#GUI END
 root.mainloop()
